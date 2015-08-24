e01de7888258ef1b81f808a3bbc82b33a0a3dc39
Mary Dickson <mdickson@gmail.com>
2015-08-24 23:45:36 +0000

    move micropost stub

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,7 @@ User.create!(name:      "Example User",
   users = User.order(:created_at).take(6)
   50.times do
     content = Faker::Lorem.sentence(5)
-    users.each {|user| user.microposts.create!(content: content)
-    }
+    users.each { |user| user.microposts.create!(content: content) }
   end
   
 end
